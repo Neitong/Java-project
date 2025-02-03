512123cc83f18e611d2a1a9a132ada603b823eaf
Meakputsoktheara Mony <meakputsoktheara.mony@student.cadt.edu.kh>
2025-02-03 21:10:51 +0700

    constructor

---
+++ addtocart/addtocart.java
@@ -0,0 +1,11 @@
+class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
