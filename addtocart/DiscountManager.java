96b57bfff2c8cffd7e0548e49e0cd71c7b116199
RaFat <manrafat2006@gmail.com>
2025-03-25 17:32:26 +0700

    Resolved conflict

---
+++ addtocart/DiscountManager.java
@@ -0,0 +1,29 @@
+package com.shopping.service;
+
+public class DiscountManager {
+    private double discountPercent;
+
+    public DiscountManager() {
+        this.discountPercent = 0.0;
+    }
+
+    public boolean applyDiscount(double percentOff) {
+        if (percentOff < 0 || percentOff > 100) {
+            return false;
+        }
+        this.discountPercent = percentOff;
+        return true;
+    }
+
+    public void clearDiscount() {
+        this.discountPercent = 0.0;
+    }
+
+    public double calculateDiscount(double amount) {
+        return amount * (discountPercent / 100.0);
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+}
